c682e94f07a23b10988cf70cfbfdb9221463bded
begimaiz <117903250+begimaiz@users.noreply.github.com>
2022-11-10 17:09:38 +0600

    methods added

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def draw_board():
+    print('draw board')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def take_input():
+    print('take input')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def check_win():
+    print('check win')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def main():
+    draw_board()
+    take_input()
+    check_win()
+
+
+main()
