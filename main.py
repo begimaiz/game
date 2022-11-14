7db205e97dcd90c04db113474a1e8712b269bd4b
Tolgonai022002 <104982690+Tolgonai022002@users.noreply.github.com>
2022-11-14 18:34:10 +0600

    test remote

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 board = list(range(1, 10))
 win_coord = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
 
-
+#  test
 def draw_board():
     print("_____________")
     for i in range(3):
